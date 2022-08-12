c2cf34211fc4f477e99fb96cb227424c4f50d588
unknown <belalelmeligy516@gmail.com>
2022-08-12 14:51:02 +0200

    Register UserService and UserController in UserModule

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,12 +2,14 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { StatusEntity } from "../status/status.entity";
 import { TaskEntity } from "../task/task.entity";
+import { UserController } from "./user.controller";
 import { UserEntity } from "./user.entity";
+import { UserService } from "./user.service";
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [UserController],
+  providers: [UserService],
   exports: [],
 })
 
