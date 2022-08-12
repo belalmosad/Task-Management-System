3db4b05da8dc94dcac7b67252c3a02c0c1b5ec8c
unknown <belalelmeligy516@gmail.com>
2022-08-12 14:46:43 +0200

    Implement user service

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UserEntity } from "./user.entity";
+
+@Injectable()
+export class UserService {
+
+    async findAll() {
+        const users = await UserEntity.find();
+        return users
+    }
+
+    async addOne(newUserData: CreateUserDto) {
+        const {
+            username,
+            password
+        } = newUserData
+        const newUser = new UserEntity();
+        newUser.username = username;
+        newUser.password = password
+        await newUser.save();
+        return newUser;
+    }
+}
