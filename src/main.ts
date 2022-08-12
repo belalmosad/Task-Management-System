34abf5ff3a55b3827727dd43224a90ee9d0d4837
unknown <belalelmeligy516@gmail.com>
2022-08-12 17:09:36 +0200

    Add swagger api documentation for API

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Task management system')
+    .setDescription('API Documentation for Task Management System')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
