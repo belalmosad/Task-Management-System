f8b57d48ab3770d76139e9c78e5d4b7bd5a9da7c
unknown <belalelmeligy516@gmail.com>
2022-08-12 12:16:03 +0200

    Create user entity

---
+++ src/modules/user/user.entity.ts
@@ -0,0 +1,25 @@
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { TaskEntity } from "../task/task.entity";
+
+@Entity('user')
+export class UserEntity extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        nullable: false,
+        unique: true,
+        type: "varchar",
+        length: 50
+    })
+    username: string
+
+    @Column({
+        nullable: false,
+        select: false
+    })
+    password: string;
+
+    @OneToMany(() => TaskEntity, taskEntity => taskEntity.user)
+    tasks: TaskEntity[];
+}
