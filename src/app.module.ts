02fdc8d9b9f4a95256a8f82a986095ad7942aba0
unknown <belalelmeligy516@gmail.com>
2022-08-12 13:38:14 +0200

    Add TypeOrmModule in App.module imports and provide database details

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './modules/user/user.entity';
+import { TaskEntity } from './modules/task/task.entity';
+import { StatusEntity } from './modules/status/status.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      entities: [
+        UserEntity,
+        TaskEntity,
+        StatusEntity
+      ],
+      database: 'task-management-system',
+      synchronize: true
+    })
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
