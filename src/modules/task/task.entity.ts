7a4e6a9333d564d64e133bfa332faaa5c707f8ec
unknown <belalelmeligy516@gmail.com>
2022-08-12 12:16:50 +0200

    Create task entity

---
+++ src/modules/task/task.entity.ts
@@ -0,0 +1,22 @@
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { StatusEntity } from "../status/status.entity";
+import { UserEntity } from "../user/user.entity";
+
+@Entity('task')
+export class TaskEntity extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        nullable: false,
+        type: "varchar",
+    })
+    title: string
+
+    @ManyToOne(() => UserEntity, userEntity => userEntity.tasks)
+    user: UserEntity
+
+    @OneToOne(() => StatusEntity, statusEntity => statusEntity.id)
+    @JoinColumn()
+    status: StatusEntity
+}
