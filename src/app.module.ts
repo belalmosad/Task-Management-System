99b4a7ce0af05a420ba8687894408edeaddeb92c
unknown <belalelmeligy516@gmail.com>
2022-08-14 17:14:44 +0200

    Fix environment variable to be DB_USERNAME instead of UERNAME

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { TaskModule } from './modules/task/task.module';
       type: 'postgres',
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT),
-      username: process.env.USERNAME,
+      username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       entities: [
         UserEntity,
